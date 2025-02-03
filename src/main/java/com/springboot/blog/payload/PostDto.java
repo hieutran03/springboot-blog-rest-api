d259deabf9961165b2857be7a3998be0d5d46694
@hieutran03 <hieumdg@gmail.com>
2025-02-03 16:02:32 +0700

    Refactoring GetPostById and GetAllPosts REST API

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,10 +2,13 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    List <CommentDto> comments;
 }
